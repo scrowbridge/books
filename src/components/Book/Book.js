9db3323ec695aa19c5218e84bc4c760be2207a3b
Sara Crowbridge <sara.crowbridge@hotmail.com>
2021-02-25 12:29:45 -0600

    Added Images

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,25 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import './Book.scss';
+import axios from "axios";
 
-const Book = (props) => (
-    <div className="Book">
-        <h2>{props.title}</h2>
-        <p dangerouslySetInnerHTML={{__html: props.body}} ></p>
-        <p>{props.featured_media}</p>
-    </div>
-);
 
-Book.propTypes = {
-    title: PropTypes.string,
-    body: PropTypes.string,
-    featured_media: PropTypes.string,
-};
+export class Book extends Component {
 
-Book.defaultProps = {
-    title: "Book Title",
-    body: "Book Body.",
-    featured_media: "Book image",
-};
+    state = {
+        pics: [],
+        isLoaded: false,
+    }
 
-export default Book;
+    componentDidMount() {
+        axios
+            .get(this.props.featured_media)
+            .then((res) => {
+                console.log('Response', res);
+                //replace array in state
+                this.setState({
+                    pics: res.data.source_url,
+                    isLoaded: true,
+                });
+            })
+            .catch((err) => {
+                console.log('API error', err);
+            });
+    }
+
+    render() {
+        return (
+            <div className="Book">
+                 <h2>{this.props.title}</h2>
+                 <p dangerouslySetInnerHTML={{__html: this.props.body}}></p>
+                 <p><img src={this.state.pics}/></p>
+            </div>
+        ) // end return
+    }
+
+
+
+}
+
+
+//const Book = (props) => (
+ //   <div className="Book">
+ //       <h2>{props.title}</h2>
+ //       <p dangerouslySetInnerHTML={{__html: props.body}} ></p>
+ //       <p>{props.featured_media}</p>
+ //   </div>
+//);
+
+//Book.propTypes = {
+ //   title: PropTypes.string,
+ //   body: PropTypes.string,
+ //   featured_media: PropTypes.string,
+//};
+
+//Book.defaultProps = {
+//    title: "Book Title",
+//    body: "Book Body.",
+//    featured_media: "Book image",
+//};
+
+
--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,16 +1,15 @@
 import React, {Component} from 'react';
 import './Books.scss';
-import Book from "../Book/Book";
+import {Book} from "../Book/Book";
 import axios from "axios";
 
 export class Books extends Component {
-    booksEndpoint = 'https://scrowbridge.bitlampsites.com/wpd/final/wordpress/wp-json/wp/v2/book';
+    booksEndpoint = 'https://scrowbridge.bitlampsites.com/wpd/final/wordpress/wp-json/wp/v2/books';
     picsEndpoint = 'https://scrowbridge.bitlampsites.com/wpd/final/wordpress/wp-json/wp/v2/media';
 
     state = {
-        books: [
-
-        ],
+        books: [],
+        isLoaded: false,
 
     }
 
@@ -25,19 +24,7 @@ export class Books extends Component {
                     books: res.data,
                     isLoaded: true,
                 });
-                axios
-                    .get(this.picsEndpoint)
-                    .then((res) => {
-                        console.log('Response', res);
-                        //replace array in state
-                        this.setState({
-                            pics: res.data.featured_media,
-                            isLoaded: true,
-                        });
-                    })
-                    .catch((err) => {
-                        console.log('API error', err);
-                    });
+
             })
             .catch((err) => {
                 console.log('API error', err);
@@ -49,7 +36,10 @@ export class Books extends Component {
             <div className="books">
                 {this.state.isLoaded ? '' : <p>Loading...</p>}
                 {this.state.books.map((book) =>
-                    <Book title={book.title.rendered} body={book.content.rendered} key={book.id} featured_media={book.featured_media.rendered}/>
+                    <Book title={book.title.rendered}
+                          body={book.content.rendered}
+                          key={book.id}
+                          featured_media={book._links['wp:featuredmedia'][0].href}/>
                 )}
             </div>
         ) // end return
