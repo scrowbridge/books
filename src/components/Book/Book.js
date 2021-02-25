48bcb59dc6436b0e8dcfb74050a691168a1e9d6f
Sara Crowbridge <sara.crowbridge@hotmail.com>
2021-02-25 11:15:00 -0600

    Added Styles

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -6,17 +6,20 @@ const Book = (props) => (
     <div className="Book">
         <h2>{props.title}</h2>
         <p dangerouslySetInnerHTML={{__html: props.body}} ></p>
+        <p>{props.featured_media}</p>
     </div>
 );
 
 Book.propTypes = {
     title: PropTypes.string,
     body: PropTypes.string,
+    featured_media: PropTypes.string,
 };
 
 Book.defaultProps = {
     title: "Book Title",
-    body: "Book Body."
+    body: "Book Body.",
+    featured_media: "Book image",
 };
 
 export default Book;
--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -5,6 +5,8 @@ import axios from "axios";
 
 export class Books extends Component {
     booksEndpoint = 'https://scrowbridge.bitlampsites.com/wpd/final/wordpress/wp-json/wp/v2/book';
+    picsEndpoint = 'https://scrowbridge.bitlampsites.com/wpd/final/wordpress/wp-json/wp/v2/media';
+
     state = {
         books: [
 
@@ -23,6 +25,19 @@ export class Books extends Component {
                     books: res.data,
                     isLoaded: true,
                 });
+                axios
+                    .get(this.picsEndpoint)
+                    .then((res) => {
+                        console.log('Response', res);
+                        //replace array in state
+                        this.setState({
+                            pics: res.data.featured_media,
+                            isLoaded: true,
+                        });
+                    })
+                    .catch((err) => {
+                        console.log('API error', err);
+                    });
             })
             .catch((err) => {
                 console.log('API error', err);
@@ -34,7 +49,7 @@ export class Books extends Component {
             <div className="books">
                 {this.state.isLoaded ? '' : <p>Loading...</p>}
                 {this.state.books.map((book) =>
-                    <Book title={book.title.rendered} body={book.content.rendered} key={book.id}/>
+                    <Book title={book.title.rendered} body={book.content.rendered} key={book.id} featured_media={book.featured_media.rendered}/>
                 )}
             </div>
         ) // end return
